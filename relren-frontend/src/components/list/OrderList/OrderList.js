7c8745ce7c02ab6ced49b71bf9f0462251e04473
lee young-in <reiui9@naver.com>
2018-12-17 22:19:41 +0900

    modify orderpage

--- relren-frontend/src/components/list/OrderList/OrderList.js
+++ relren-frontend/src/components/list/OrderList/OrderList.js
@@ -31,22 +31,14 @@ const OrderItem = ({url,title,content}) => {
 
 const OrderList = () => {
   const posts = new List([
-    {url:'https://m.media-amazon.com/images/I/917LEZ+it3L._AC_SX200_SY200_.jpg',
-      text: "Drawings me opinions returned absolute in. Friend are day own either lively new. ",
-      title: "Camera"
+    {url:'https://images-na.ssl-images-amazon.com/images/I/61ecsEuu6-L._AC_.jpg',
+      text: "RESERVED",
+      title: "Nintendo Switch"
     },
     {url:'https://images-na.ssl-images-amazon.com/images/I/41Cv21vkL9L._AC_SY200_.jpg',
-      text: "Otherwise therefore sex did are unfeeling something. Certain be ye amiable by exposed so. ",
-      title: "Headset"
-    },
-    {url:'https://images-na.ssl-images-amazon.com/images/I/51I%2BteGzpCL._AC_SY200_.jpg',
-      text: "Coming either suffer living her gay theirs. Furnished do otherwise daughters contented conveying attempted no.",
-      title: "Joystick"
-    },
-    {url:'https://images-na.ssl-images-amazon.com/images/I/41Cq%2BW%2BIooL._AC_SY200_.jpg',
-      text: "Friend are day own either lively new. ",
-      title: "Laptop"
-    },
+      text: "ON DELIVERY",
+      title: "Headset x1"
+    }
    ]);
 
    const orderList = posts.map(post => {
